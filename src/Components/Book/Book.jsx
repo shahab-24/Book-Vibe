6aa72da4ac821c71863f7b7adefaf8a65136e2ea
Shahab Uddin <shahab.82946@gmail.com>
2024-03-26 07:38:06 +0600

    added imgbb image

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,16 +1,18 @@
-
-
-
-const Book = ({book}) => {
-  const {image,tags,bookName,author,category,rating} = book;
+const Book = ({ book }) => {
+  const { image, tags, bookName, author, category, rating } = book;
   return (
-    <div>
-      <img src={image}></img>
-      <p>{tags}</p>
-      <h2>{bookName}</h2>
+    <div className="book">
+      <img src={image} className="w-[80%] text-center"></img>
+      <div className="flex justify-between">
+      <p className="text-[chartreuse]">{book.tags.join(', ')}</p>
+    
+      </div>
+      <h2 className="text-2xl font-bold">{bookName}</h2>
       <h2>{author}</h2>
-      <p>{category}</p>
-      <p>{rating}</p>
+      <div className="flex justify-between">
+        <h3>{category}</h3>
+        <h3>{rating}</h3>
+      </div>
     </div>
   );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Book from "../Book/Book";
 
 const Home = () => {
-  const [books, setBooks] = useState([6]);
+  const [books, setBooks] = useState([]);
   useEffect(() => {
     fetch("Books.json")
       .then((res) => res.json())
@@ -12,11 +12,11 @@ const Home = () => {
 
   return (
     <div>
-      <h1>books{books.length}</h1>
-      <div className="grid md:grid-cols-2 gap-4 max-w-5xl mx-auto">
-        {
-          books.map(book => <Book key={book.Id} book={book}></Book>)
-        }
+      <h1 className="text-center font-extrabold text-3xl mb-6 mt-10">Books</h1>
+      <div className="grid md:grid-cols-3 gap-6 max-w-5xl mx-auto">
+        {books.map((book) => (
+          <Book key={book.Id} book={book}></Book>
+        ))}
       </div>
     </div>
   );
