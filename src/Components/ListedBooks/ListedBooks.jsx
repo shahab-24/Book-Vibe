24da8385f0adfd55236c8a87fb8c514b021c5216
Shahab Uddin <shahab.82946@gmail.com>
2024-03-29 12:30:42 +0600

    modified listed book page

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -55,13 +55,10 @@ const ListedBooks = () => {
 
   return (
     
-      <div className='px-6'>
+      <div className='px-6 mx-auto max-w-5xl'>
        
        
-         
-        
-       
-       <h1 className="text-3xl font-bold mt-20">Listed Books: {findBooks.length}</h1>
+       <h1 className="text-3xl font-bold mt-20 text-center">Books: {findBooks.length}</h1>
         <div className="flex flex-col justify-center items-center mt-6">
         <h1 className="bg-green-500 text-black text-center mt-6 mb-2 font-bold py-2 px-4 rounded w-40">Sort BY</h1>
         <select value={sortBy} onChange={handleSortChange} className="mr-4">
@@ -74,7 +71,7 @@ const ListedBooks = () => {
       
 
 
-        <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap text-gray-100">
+        <div className="flex items-center mx-auto max-w-5xl overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap text-gray-100">
 	<Link to='' onClick={() => setTabIndex(0)} rel="noopener noreferrer" href="#" className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-400 text-black`}>
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
 			<path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
@@ -98,24 +95,37 @@ const ListedBooks = () => {
 
 
 
-<div className="grid md:grid-cols-3 gap-4 mt-4 ">
+<div className="gap-4 py-4 mt-4 mx-auto max-w-5xl ">
         {displayBooks.map(book => (
-          <div key={book.id} className="border p-4 shadow-xl rounded-xl">
-            <div className='flex justify-center text-center items-center rounded-xl py-4'><img src={book.image} alt={book.bookName} /></div>
+          <div key={book.id} className="border py-6 mb-4 p-4 shadow-xl flex  rounded-xl">
+            <div className='text-center px-4 w-[230px] h-auto items-center rounded-xl py-4'><img src={book.image} alt={book.bookName} /></div>
+           <div className='px-4 py-4 space-y-6'>
+           <div className='py-4 space-y-4'>
             <h1 className='text-3xl font-bold'>{book.bookName}</h1>
-            <h2 className='text-xl font-bold'>{book.author}</h2>
-            <p>Category: {book.category}</p>
-            <div className='flex justify-between text-green-500 font-bold'>
-            <p>#{book.tags[0]}</p>
-            <p>#{book.tags[1]}</p>
+            <h2 className='text-xl font-bold'>By: {book.author}</h2>
+            
             </div>
-            <p>Total Pages: {book.totalPages}</p>
-            <p>Publisher: {book.publisher}</p>
-            <p>Publish Year: {book.yearOfPublishing}</p>
+            <div className='flex justify-around space-x-4 text-green-500 font-bold'>
+            <h1>#{book.tags[0]}</h1>
+            <h1>#{book.tags[1]}</h1>
+            <div>
+            <h1 className='text-black'>Publish Year: {book.yearOfPublishing}</h1></div>
+            </div>
+            
+            <div className='flex justify-between'>
+            <p><small>Total Pages: {book.totalPages}</small></p>
+            <p><small>Publisher: {book.publisher}</small></p>
+            </div>
+            
+            <div className='flex justify-around items-center'>
+            <p style={{borderRadius: '30px'}}>Category: {book.category}</p>
             <p>Rating: {book.rating}</p>
             <Link to=''>
        <button onClick={()=> handleViewDetails()} className="bg-green-500 hover:bg-green-700 text-white mt-6 font-bold py-2 px-4 rounded">View Details</button>
        </Link>
+            </div>
+           </div>
+       
           </div>
         ))}
       </div>
